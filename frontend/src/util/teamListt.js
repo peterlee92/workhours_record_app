bb91fb19aa0d74a6d73965df674308f74fa6ec0d
peterlee92 <mhyeok92@gmail.com>
2021-07-13 20:17:34 -0700

    made select list

---
+++ frontend/src/util/teamListt.js
@@ -0,0 +1,14 @@
+export const teamList = [
+    {
+        name: 'Team',
+        value: null
+    },
+    {
+        name: 'Site Centre',
+        value: 'Site Centre'
+    }, 
+    {
+        name: 'Techies of Tomorrow',
+        value: 'Techies of Tomorrow'
+    }
+]
